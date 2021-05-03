7d0ff032d436f796bb7061bbe26b9a483454ffb9
VyacheslavSazonov <ghostly77spirit@gmail.com>
2021-05-03 22:49:50 +0300

    End HW 3

---
+++ Lab3/Collections/Collections/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab2
+{
+    abstract class Human
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public abstract void actions();
+        public abstract void leisure();
+        public void eat() 
+        {
+            Console.WriteLine("Ням");
+        }
+        public void sleep() 
+        {
+            Console.WriteLine("Zzzz\n");
+        }
+    }
+}
